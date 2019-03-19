bd06de07362fb99a1dc2a49e68e005571b0fa9e9
Vitya <levchenko.victor@gmail.com>
2019-03-19 22:25:03 +0200

    start swift tutorial

--- umlkaTests/swiftTour_Tests.swift
+++ umlkaTests/swiftTour_Tests.swift
@@ -12,8 +12,29 @@ class swiftTour_Tests: XCTestCase {
 
     func testExample() {
 		
-	//	XCTAssert( false );
+		// constant ----------------------
+		let PI = 3.1415
+		print("   >>> PI : \(PI)")
 		
+		// variable ----------------------
+		var log = 2323
+		print("   >>> log : \(log)")
+		log = 12
+		print("   >>> log : \(log)")
+		
+		// types -------------------------
+		let implicitInteger = 70
+		let implicitDouble  = 70.0
+		let explicitDouble: Double = 70
+		let explicitFloat: Float = 4
+		print("   >>> implicitInteger = 70 : \(implicitInteger)")
+		print("   >>> let implicitDouble  = 70.0 : \(implicitDouble)")
+		print("   >>> let explicitDouble: Double = 70 : \(explicitDouble)")
+		print("   >>> let explicitFloat: Float = 4 : \(explicitFloat)")
+		
+		
+		
+		//--------------------------------
     }
 	
 }
