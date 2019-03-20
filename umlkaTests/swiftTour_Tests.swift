05fc23cc4c174688b0f4e72f3d8196071ac7cef7
Vitya <levchenko.victor@gmail.com>
2019-03-20 17:50:51 +0200

    Class and object - done

--- umlkaTests/swiftTour_Tests.swift
+++ umlkaTests/swiftTour_Tests.swift
@@ -262,4 +262,111 @@ class swiftTour_Tests: XCTestCase {
 		
 		//--------------------------------
 	}
+	
+	func testSWIFT_Class() {
+		
+		// class -------------------------
+		class NamedShape {
+			var numberOfSides: Int = 0
+			var name: String
+			
+			init(name: String) {
+				self.name = name
+			}
+			
+			deinit {
+				print("   >>> cean after shape")
+			}
+			
+			func simpleDescription() -> String {
+				return "A shape with \(numberOfSides) sides."
+			}
+		}
+		
+		// sub-class ---------------------
+		class Square: NamedShape {
+			var sideLength: Double
+			
+			init(sideLength: Double, name: String) {
+				self.sideLength = sideLength
+				super.init(name: name)
+				numberOfSides = 4
+			}
+			
+			func area() -> Double {
+				return sideLength * sideLength
+			}
+			
+			override func simpleDescription() -> String {
+				return "A square with sides of length \(sideLength)."
+			}
+		}
+		
+		let test = Square(sideLength: 5.2, name: "my test square")
+		print("   >>> test.area() : \(test.area())")
+		print("   >>> test.simpleDescription() : \(test.simpleDescription())")
+		
+		// getter-setter -----------------
+		class EquilateralTriangle: NamedShape {
+			var sideLength: Double = 0.0
+			
+			init(sideLength: Double, name: String) {
+				self.sideLength = sideLength
+				super.init(name: name)
+				numberOfSides = 3
+			}
+			
+			var perimeter: Double {
+				get {
+					return 3.0 * sideLength
+				}
+				set (length) {
+					sideLength = length / 3.0  // implicit : newValue
+				}
+			}
+			
+			override func simpleDescription() -> String {
+				return "An equilateral triangle with sides of length \(sideLength)."
+			}
+		}
+		
+		let triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+		print("   >>> triangle.perimeter : \(triangle.perimeter)")
+		triangle.perimeter = 9.9
+		print("   >>> triangle.sideLength : \(triangle.sideLength)")
+		
+		// will-did set ------------------
+		class TriangleAndSquare {
+			var triangle: EquilateralTriangle {
+				willSet {
+					square.sideLength = newValue.sideLength
+				}
+			}
+			var square: Square {
+				willSet {
+					triangle.sideLength = newValue.sideLength
+				}
+			}
+			init(size: Double, name: String) {
+				square = Square(sideLength: size, name: name)
+				triangle = EquilateralTriangle(sideLength: size, name: name)
+			}
+		}
+		
+		let triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+		XCTAssertEqual(10.0, triangleAndSquare.square.sideLength)
+		XCTAssertEqual(10.0, triangleAndSquare.triangle.sideLength)
+		triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+		XCTAssertEqual(50.0, triangleAndSquare.triangle.sideLength)
+		
+		// nil ---------------------------
+		var optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+		var sideLength = optionalSquare?.sideLength
+		XCTAssertNotNil(sideLength)
+		optionalSquare = nil
+		sideLength = optionalSquare?.sideLength // if nill? - other just ignored = nil
+		XCTAssertNil(sideLength)
+		
+		//--------------------------------
+	}
 }
