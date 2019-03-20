dd1f76feab68bf0428607470abbdc1f50702b143
Vitya <levchenko.victor@gmail.com>
2019-03-20 19:38:07 +0200

    Error Handling - done

--- umlkaTests/swiftTour_Tests.swift
+++ umlkaTests/swiftTour_Tests.swift
@@ -524,4 +524,70 @@ class swiftTour_Tests: XCTestCase {
 		
 		//--------------------------------
 	}
+	
+	func testSWIFT_Error() {
+		
+		// Error -------------------------
+		enum PrinterError: Error {
+			case outOfPaper
+			case noToner
+			case onFire
+		}
+		
+		// throw(s) ----------------------
+		func send(job: Int, toPrinter printerName: String) throws -> String {
+			if printerName == "Never Has Toner" {
+				throw PrinterError.noToner
+			}
+			return "Job sent to \(printerName)"
+		}
+		
+		// do-catch ----------------------
+		do {
+			let printerResponse = try send(job: 1040, toPrinter: "Bi Sheng")
+			print("   >>> \(printerResponse)")
+			let failPrinterResponce = try send(job: 666, toPrinter: "Never Has Toner")
+			print("   >>> \(failPrinterResponce)")
+		} catch {
+			print("   >>> \(error)")
+		}
+		
+		// catch -------------------------
+		do {
+			let printerResponse = try send(job: 1440, toPrinter: "Gutenberg")
+			print("   >>> \(printerResponse)")
+		} catch PrinterError.onFire {
+			print("   >>> I'll just put this over here, with the rest of the fire.")
+		} catch let printerError as PrinterError {
+			print("   >>> Printer error: \(printerError).")
+		} catch {
+			print("   >>> \(error)")
+		}
+		
+		// try? --------------------------
+		let printerSuccess = try? send(job: 1884, toPrinter: "Mergenthaler")
+		let printerFailure = try? send(job: 1885, toPrinter: "Never Has Toner")
+		
+		if let info = printerSuccess { print("   >>> \(info)") }
+		if let info = printerFailure { print("   >>> \(info)") }
+		
+		// defer -------------------------
+		var fridgeIsOpen = false
+		let fridgeContent = ["milk", "eggs", "leftovers"]
+		
+		func fridgeContains(_ food: String) -> Bool {
+			fridgeIsOpen = true			// --  SETUP  --
+			defer {		/// !!!!!!! executed after all other code in the function even is throws error
+				fridgeIsOpen = false	// -- CLEANUP --
+			}
+			
+			let result = fridgeContent.contains(food)
+			return result
+		}
+		
+		print("   >>> fridgeContains(banana) : \(fridgeContains("banana"))")
+		print("   >>> fridgeIsOpen : \(fridgeIsOpen)")
+		
+		//--------------------------------
+	}
 }
